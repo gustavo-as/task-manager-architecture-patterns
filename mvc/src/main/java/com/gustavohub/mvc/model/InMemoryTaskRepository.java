406172c9c7ec75a511111606870e06f04145ea55
AS Gustavo <gustavo.simoes@hotmail.com.br>
2026-03-05 20:15:52 +0100

    add TaskRepository interface and InMemoryTaskRepository

---
+++ mvc/src/main/java/com/gustavohub/mvc/model/InMemoryTaskRepository.java
@@ -0,0 +1,42 @@
+package com.gustavohub.mvc.model;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class InMemoryTaskRepository implements TaskRepository {
+
+    private final Map<String, Task> storage = new HashMap<>();
+
+    @Override
+    public Task save(Task task) {
+        storage.put(task.getId(), task);
+        return task;
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public Optional<Task> findById(String id) {
+        return Optional.ofNullable(storage.get(id));
+    }
+
+    @Override
+    public Task update(Task task) {
+        storage.put(task.getId(), task);
+        return task;
+    }
+
+    @Override
+    public void deleteById(String id) {
+        storage.remove(id);
+    }
+}
